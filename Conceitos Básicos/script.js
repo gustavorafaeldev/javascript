230e5b75cf8a88822fbbed57b6f899b68520e930
gustavorafaeldev <105467235+gustavorafaeldev@users.noreply.github.com>
2022-05-28 15:48:00 -0300

    Finalização Exercício

---
+++ Conceitos Básicos/script.js
@@ -0,0 +1,25 @@
+function azul() {
+    limpar();
+    document.getElementById('titulo').classList.add('azul');
+}
+
+function vermelho() {
+    limpar();
+    document.getElementById('titulo').classList.add('vermelho');
+}
+
+function verde() {
+    limpar();
+    document.getElementById('titulo').classList.add('verde');
+}
+
+function limpar() {
+    document.getElementById('titulo').classList.remove('azul');
+    document.getElementById('titulo').classList.remove('vermelho');
+    document.getElementById('titulo').classList.remove('verde');
+}
+
+function mostrarTelefone(elemento) {
+    document.getElementById('telefone').style.display = 'block'
+    elemento.style.display = 'none'
+}
